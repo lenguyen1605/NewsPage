86a7de77b907159128c514ebd7202271ef1ccde5
nguy2940 <nguyennhatle1605@gmail.com>
2023-08-12 19:41:19 +0700

    s

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from datetime import date, time, datetime
 from fastapi.middleware.cors import CORSMiddleware
 from firebase_admin import firestore, credentials
+from google.cloud.firestore_v1 import FieldFilter
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 # import datetime
 from datetime import datetime
 import uuid
+import jwt
+from fastapi.encoders import jsonable_encoder
 
 cred = credentials.Certificate("./credentials.json")
 firebase_admin.initialize_app(cred)
@@ -19,6 +22,9 @@ app = FastAPI()
 origins = [
     "http://localhost:3000"
 ]
+SECRET_KEY = "c0f216988e911e0ded52ed9b9f4fca53554f9984aefeb96075ed2a94803376fd"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1000
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,6 +43,11 @@ class User(BaseModel):
     # author: False
 
 
+class LogIn(BaseModel):
+    email: str
+    password: str
+
+
 class Post(BaseModel):
     categories: List[str]
     content: str
@@ -46,6 +57,7 @@ class Post(BaseModel):
     likes: int
     title: str
     summary: str
+    id_author: str
 
 
 @app.post("/Signup")
@@ -59,6 +71,38 @@ def create_user(user_data: User):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@app.post("/Signin")
+def log_in(user_data: LogIn):
+    db = firestore.client()
+    try:
+        ar = []
+        all_users = db.collection('users').stream()
+        data = {}
+        for user in all_users:
+            user_info = user.to_dict()
+            if user_info['email'] == user_data.email:
+                if user_info['password'] == user_data.password:
+                    data = {'email': user_data.email, 'password': user_data.password, 'username': user_info['username'],
+                            'id_user': user.id}
+                    print("data", data)
+                    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+                    return {"token": encoded_jwt}
+        return {"token": "Login failed"}
+        # size = len([user for user in all_users])
+        # if size == 0:
+        #     return {"token": "Login failed"}
+        # else:
+        #     print([user for user in all_users])
+        # print("size", size)
+        # data = {
+        #     "email": user_data.email,
+        #     "password": user_data.password,
+        #
+        # }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.post("/setPost")
 def set_post(post_info: Post):
     db = firestore.client()
@@ -67,9 +111,9 @@ def set_post(post_info: Post):
         new_post = db.collection('post').document(str(random_id))
         doc_categories = [db.collection('categories').document(category) for category in post_info.categories]
         print(doc_categories)
-
         new_post.set({"content": post_info.content, "date_created": datetime.now(),
-                      "id_author": db.collection('users').document('le'), "likes": post_info.likes,
+                      "id_author": db.collection('users').document(post_info.id_author),
+                      "likes": post_info.likes,
                       "title": post_info.title, "image": post_info.image,
                       "summary": post_info.summary, "categories": doc_categories, "id": str(random_id)})
         for category in post_info.categories:
@@ -111,7 +155,6 @@ def get_all_post():
             author_name = db.collection('users').document(doc.get('id_author').id)
             data['author_name'] = author_name.get().get("username")
             # print(data)
-            print(data)
             all_posts.append(data)
         # print(all_posts)
         return all_posts
@@ -162,9 +205,7 @@ def get_post_by_category(category):
         all_posts = []
 
         cat_ref = db.collection('categories').document(category)
-        print("cat_ref", cat_ref)
         cat = cat_ref.get().to_dict()
-        print("cat here", cat)
         for post in cat["posts"]:
             data = post.get().to_dict()
             data["id_author"] = post.get().get("id_author").id
@@ -186,9 +227,7 @@ def get_post_by_category(category):
             author_name = db.collection('users').document(post.get().get('id_author').id)
 
             data['author_name'] = author_name.get().get("username")
-            print("data", data)
             all_posts.append(data)
-        print("all_post", all_posts)
         return all_posts
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
